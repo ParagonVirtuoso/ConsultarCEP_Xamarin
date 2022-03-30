147d9e5e8337315cc859791b9992289d87694914
Denys Fernandes <38758938+denysfer@users.noreply.github.com>
2022-03-30 05:13:32 -0300

    Initial commit

---
+++ App1_ConsultarCEP/App1_ConsultarCEP/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using App1_ConsultarCEP.Servico;
+using App1_ConsultarCEP.Servico.Modelo;
+
+namespace App1_ConsultarCEP
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+            
+			InitializeComponent();
+
+            BotaoId.Clicked += BuscarCEP;
+           
+            
+		}
+        private void BuscarCEP(object sender, EventArgs args) {
+
+            //pegar o texto digitado na caixa 
+            //o trim para retirar todos os espacos em branco
+            string cep = CepId.Text.Trim();
+
+            //TODO - Lógica do programa quando é clicado no botao
+            Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+
+           RESULTADO.Text = string.Format("Endereço: {2} de {3} {0}, {1}", end.localidade , end.uf, end.logradouro, end.bairro);    
+
+
+        }
+	}
+}
---
+++ App1_ConsultarCEP/App1_ConsultarCEP/Servico/ViaCEPServico.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using App1_ConsultarCEP.Servico.Modelo; //para poder usar a classe anterior e definir cada item como objeto
+using Newtonsoft.Json; //necessita intalar pelo nuget para usar essa biblio
+
+namespace App1_ConsultarCEP.Servico
+{
+    public class ViaCEPServico
+    {
+        //o {0} é para posteriormente ser substituido pelo cep
+        private static string EnderecoUrl = "https://viacep.com.br/ws/{0}/json/";
+           //usando como Endereco para que possa retornar
+        public static Endereco BuscarEnderecoViaCEP(String cep) {
+            //primeiro parametro que é a url e o segundo é o cep que o usuario digitou
+            string NovoEnderecoURL = string.Format(EnderecoUrl, cep);
+
+            //cliente web necessario usar a biblioteca System.Net
+            WebClient wc = new WebClient();
+            //fazer com que o webclientfaca o download dependendo dos metodos
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            //metodo para descerializar a informacao
+                            //ele vai quebrar o objeto Endereco e vai armazenar em Conteudo
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+            if (end.cep == null) return null;
+            //retorno da variavel endereco que tem o conteudo e tenha o objeto com as informacoes
+            return end;
+        }
+
+    }
+}
